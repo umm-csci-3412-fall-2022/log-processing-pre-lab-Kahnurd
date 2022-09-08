7a52d231137f93ef203b1b17566f3fd1fd88dd1c
Kahnurd <hetti031@morris.umn.edu>
2022-09-07 23:42:28 -0500

    Start on regex.sh. It has the general outline, just needs better regex

---
+++ regex/regex.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+first_case=r0_input.txt
+second_case=r1_input.txt
+third_case=r2_input.txt
+
+touch r0_output.txt
+touch r1_output.txt
+touch r2_output.txt
+
+
+#awk -e '$2 ~ /^K/ {print $0}' "$first_case"
+
+
+awk 'match ($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n"; }' < "$first_case" > r0_output.txt
+
+awk 'match($0, /([A-Z][a-zA-Z]+), ([a-z]+\.)/, words) {print "1. " words[1] "\n" "2. " words[2] "\n"}' < "$second_case" > r1_output.txt
+
+awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+\.[a-zA-Z]\.[a-zA-Z])/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < "$third_case" > r2_output.txt
