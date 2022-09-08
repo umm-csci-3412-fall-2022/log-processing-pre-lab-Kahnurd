5024ca941dc163fc19ab2bb136ce5f20ee7f605f
Kahnurd <hetti031@morris.umn.edu>
2022-09-07 19:54:48 -0500

    Go through examples of using bash command line arguments. Temporarily use the wrap_content.sh file to follow along with the demos.

--- wrap_contents/wrap_content.sh
+++ wrap_contents/wrap_content.sh
@@ -4,4 +4,33 @@ content_file=$1
 headNfoot_name=$2
 file_name=$3
 
+Name="Fish"
 
+help(){
+	# Display Help
+	echo "Add description of the script functions here"
+	echo
+	echo "Syntax: scriptTemplate: [-g|h|v|V]"
+	echo "Options:"
+	echo "g     Print the GPL liscense notification"
+	echo "h     Print this help"
+	echo "v     Verbose mode"
+	echo "V     Print software version and exit"
+	echo
+}
+
+while getopts ":hn:" option; do
+	case $option in
+		h) #Display help
+			help
+			exit;;
+		n)
+			Name=$OPTARG;;
+		\?) # Invalid option
+			echo "Error: Invalid option '$content_file'"
+			exit;;
+
+	esac
+done
+
+echo "A salmon named: $Name"
